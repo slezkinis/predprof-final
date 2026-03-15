a94ef75f5bb90f8ed5b7a292dec68a065d9d5c04
slezkinis <slezkin.is@mail.ru>
2026-03-15 10:47:35 +0300

    Add decoder

---
+++ ml/decode_data.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import pickle
+
+
+train_y = np.load(input("Введите путь до train_y: "), allow_pickle=True)
+labels = np.array([s[32:] for s in train_y])
+encoder = LabelEncoder()
+train_y_encoded = encoder.fit_transform(labels)
+np.save("train_y_encoded.npy", train_y_encoded)
+with open("label_encoder.pkl", "wb") as f:
+    pickle.dump(encoder, f)
+
+print("Готово!")
+print("Созданные файлы:")
+print("train_y_encoded.npy")
+print("label_encoder.pkl")
+print("Количество классов:", len(encoder.classes_))
