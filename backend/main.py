1dcd5624f9e13ef3c7d4263a929507f9b1566cd2
Dima123kr <krivosheya_da@mail.ru>
2026-03-15 12:24:15 +0300

    Backend 2.0
    Добавил получение файла на главной странице

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from data import db_session
 from data.user import User
 from forms.register import RegisterForm
@@ -7,7 +7,7 @@ from flask_login import LoginManager, login_user, login_required, logout_user, c
 from data.db_functions import new_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "secret_key"
+app.config["SECRET_KEY"] = "secret_key"
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -18,33 +18,36 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
-@app.route('/', methods=['GET', "POST"])
+@app.route("/", methods=["GET", "POST"])
 def main_page():
-    return render_template('main_page.html')
+    if request.method == "POST":
+        file = request.files["file"]
+        return render_template("main.html", result="100")
+    return render_template("main.html")
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 @login_required
 def register():
     if not current_user.is_admin:
-        return redirect('/')
+        return redirect("/")
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
-            return render_template('register.html', form=form, message="Пароли не совпадают")
+            return render_template("register.html", form=form, message="Пароли не совпадают")
         db_sess = db_session.create_session()
         if db_sess.query(User).get(User.email == form.email.data):
-            return render_template('register.html', form=form, message="Указанная почта занята")
+            return render_template("register.html", form=form, message="Указанная почта занята")
 
         user = new_user(form.email.data, form.password.data, form.first_name.data, form.last_name.data)
 
         db_sess.add(user)
         login_user(user)
-        return redirect('/')
-    return render_template('register.html', form=form)
+        return redirect("/")
+    return render_template("register.html", form=form)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -53,25 +56,25 @@ def login():
         if user:
             login_user(user)
             return redirect("/")
-        return render_template('login.html', message="Неправильный логин или пароль", form=form)
-    return render_template('login.html', form=form)
+        return render_template("login.html", message="Неправильный логин или пароль", form=form)
+    return render_template("login.html", form=form)
 
 
-@app.route('/account')
+@app.route("/account")
 @login_required
 def account():
-    return render_template('account.html', user=current_user)
+    return render_template("account.html", user=current_user)
 
 
-@app.route('/account/leave')
+@app.route("/account/leave")
 @login_required
 def leave_account():
     logout_user()
-    return redirect('/')
+    return redirect("/")
 
 
 db_session.global_init("db/db.db")
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8888, debug=False)
+    app.run(host="127.0.0.1", port=8888, debug=False)
